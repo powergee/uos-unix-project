b43eac6441edbbd67294a11ab0be24de5c8fbb12
Jeonghyeon Kim <powergee101@gmail.com>
2020-11-30 00:25:45 +0900

    Reformat indentations

--- mini_sh.c
+++ mini_sh.c
@@ -14,88 +14,106 @@
 #define FALSE 0
 #define TRUE 1
 
-#define EOL	1
-#define ARG	2
+#define EOL 1
+#define ARG 2
 #define AMPERSAND 3
 
 #define FOREGROUND 0
 #define BACKGROUND 1
 
-static char	input[512];
-static char	tokens[1024];
-char		*ptr, *tok;
+static char input[512];
+static char tokens[1024];
+char *ptr, *tok;
 
 int get_token(char **outptr)
 {
-	int	type;
+	int type;
 
 	*outptr = tok;
-	while ((*ptr == ' ') || (*ptr == '\t')) ptr++;
+	while ((*ptr == ' ') || (*ptr == '\t'))
+		ptr++;
 
 	*tok++ = *ptr;
 
-	switch (*ptr++) {
-		case '\0' : type = EOL; break;
-		case '&': type = AMPERSAND; break;
-		default : type = ARG;
-			while ((*ptr != ' ') && (*ptr != '&') &&
-				(*ptr != '\t') && (*ptr != '\0'))
-				*tok++ = *ptr++;
+	switch (*ptr++)
+	{
+	case '\0':
+		type = EOL;
+		break;
+	case '&':
+		type = AMPERSAND;
+		break;
+	default:
+		type = ARG;
+		while ((*ptr != ' ') && (*ptr != '&') &&
+			   (*ptr != '\t') && (*ptr != '\0'))
+			*tok++ = *ptr++;
 	}
 	*tok++ = '\0';
-	return(type);
+	return (type);
 }
 
 int execute(char **comm, int how)
 {
-	int	pid;
+	int pid;
 
-	if ((pid = fork()) < 0) {
+	if ((pid = fork()) < 0)
+	{
 		fprintf(stderr, "minish : fork error\n");
-		return(-1);
+		return (-1);
 	}
-	else if (pid == 0) {
+	else if (pid == 0)
+	{
 		execvp(*comm, comm);
 		fprintf(stderr, "minish : command not found\n");
 		exit(127);
 	}
-	if (how == BACKGROUND) {	/* Background execution */
+	if (how == BACKGROUND)
+	{ /* Background execution */
 		printf("[%d]\n", pid);
 		return 0;
-	}		
+	}
 	/* Foreground execution */
 	while (waitpid(pid, NULL, 0) < 0)
-		if (errno != EINTR) return -1;
+		if (errno != EINTR)
+			return -1;
 	return 0;
 }
 
 int parse_and_execute(char *input)
 {
-	char	*arg[1024];
-	int	type, how;
-	int	quit = FALSE;
-	int	narg = 0;
-	int	finished = FALSE;
+	char *arg[1024];
+	int type, how;
+	int quit = FALSE;
+	int narg = 0;
+	int finished = FALSE;
 
 	ptr = input;
 	tok = tokens;
-	while (!finished) {
-		switch (type = get_token(&arg[narg])) {
-		case ARG :
+	while (!finished)
+	{
+		switch (type = get_token(&arg[narg]))
+		{
+		case ARG:
 			narg++;
 			break;
-		case EOL :
+		case EOL:
 		case AMPERSAND:
-			if (!strcmp(arg[0], "quit")) quit = TRUE;
-			else if (!strcmp(arg[0], "exit")) quit = TRUE;
-			else if (!strcmp(arg[0], "cd")) {
+			if (!strcmp(arg[0], "quit"))
+				quit = TRUE;
+			else if (!strcmp(arg[0], "exit"))
+				quit = TRUE;
+			else if (!strcmp(arg[0], "cd"))
+			{
 				/* Do something */
 			}
-			else if (!strcmp(arg[0], "type")) {
-				if (narg > 1) {
-					int	i, fid;
-					int	readcount;
-					char	buf[512];
+			else if (!strcmp(arg[0], "type"))
+			{
+				if (narg > 1)
+				{
+					int i, fid;
+					int readcount;
+					char buf[512];
 					/*  학생들이 프로그램 작성할 것
 					fid = open(arg[1], ...);
 					if (fid >= 0) {
@@ -110,7 +128,8 @@ int parse_and_execute(char *input)
 					*/
 				}
 			}
-			else {
+			else
+			{
 				how = (type == AMPERSAND) ? BACKGROUND : FOREGROUND;
 				arg[narg] = NULL;
 				if (narg != 0)
@@ -119,25 +138,23 @@ int parse_and_execute(char *input)
 			narg = 0;
 			if (type == EOL)
 				finished = TRUE;
-			break; 
+			break;
 		}
 	}
 	return quit;
 }
 
-
-
 main()
 {
-    	char	*arg[1024];
-	int	quit;
+	char *arg[1024];
+	int quit;
 
 	printf("msh # ");
-	while (gets(input)) {
+	while (gets(input))
+	{
 		quit = parse_and_execute(input);
-		if (quit) break;
+		if (quit)
+			break;
 		printf("msh # ");
 	}
 }
-
-
